fb9c917439fd3afd93e504f8989059e51d78c401
gglaochen <925576956@qq.com>
2020-07-30 21:47:42 +0800

    解决日志包冲突等问题

--- src/main/java/com/example/flink/flinkdemo/FlinkDemoApplication.java
+++ src/main/java/com/example/flink/flinkdemo/FlinkDemoApplication.java
@@ -3,7 +3,7 @@ package com.example.flink.flinkdemo;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer08;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer010;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -16,11 +16,11 @@ public class FlinkDemoApplication {
         SpringApplication.run(FlinkDemoApplication.class, args);
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         Properties properties = new Properties();
-        properties.setProperty("bootstrap.servers", "localhost:9092");
+        properties.setProperty("bootstrap.servers", "192.168.1.221:9092");
         // only required for Kafka 0.8
-        properties.setProperty("zookeeper.connect", "localhost:2181");
+        properties.setProperty("zookeeper.connect", "192.168.1.221:2181");
         properties.setProperty("group.id", "test");
-        DataStream<String> stream = env.addSource(new FlinkKafkaConsumer08<>("topic", new SimpleStringSchema(), properties));
+        DataStream<String> stream = env.addSource(new FlinkKafkaConsumer010<>("topic", new SimpleStringSchema(), properties));
         stream.print().setParallelism(1);
         env.execute("test");
     }
