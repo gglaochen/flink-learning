cd65dfc31819c1a2142de3f50baf38c3dda55284
gglaochen <925576956@qq.com>
2020-08-03 21:41:45 +0800

    *add 合流 *add 分流

--- src/main/java/com/example/flink/flinkdemo/FlinkDemoApplication.java
+++ src/main/java/com/example/flink/flinkdemo/FlinkDemoApplication.java
@@ -2,12 +2,18 @@ package com.example.flink.flinkdemo;
 
 import com.example.flink.flinkdemo.source.DiySource;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.streaming.api.collector.selector.OutputSelector;
+import org.apache.flink.streaming.api.datastream.ConnectedStreams;
 import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.SplitStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.co.CoMapFunction;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer010;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 @SpringBootApplication
@@ -25,10 +31,51 @@ public class FlinkDemoApplication {
         DataStream<String> stream = env.addSource(new FlinkKafkaConsumer010<>("topic", new SimpleStringSchema(), properties));
 
         /* 2.自定义 Source*/
-
         DataStream<Double> stream2 = env.addSource(new DiySource());
 
-        stream2.print().setParallelism(1);
+        /**
+         * 根据hashCode分组
+         */
+        DataStream<Double> stream3 = stream2.keyBy(x -> x).sum(0);
+        DataStream<Double> stream4 = stream2.keyBy(x -> x).reduce(Double::sum);
+        /**
+         * splite分流及select选择流(已过时)
+         */
+        SplitStream<Double> stream5 = stream2.split((OutputSelector<Double>) value -> {
+            List<String> output = new ArrayList<>();
+            if (value instanceof Double) {
+                output.add("double");
+            } else {
+                output.add("notDouble");
+            }
+            return output;
+        });
+        DataStream<Double> isDouble = stream5.select("double");
+        DataStream<Double> notDouble = stream5.select("notDouble");
+
+        /**
+         * collect + map合并流
+         */
+        ConnectedStreams<Double, Double> collectStream = isDouble.connect(notDouble);
+        DataStream<Double> mapStream = collectStream.map(new CoMapFunction<Double, Double, Double>() {
+            @Override
+            public Double map1(Double value) throws Exception {
+                return value;
+            }
+
+            @Override
+            public Double map2(Double value) throws Exception {
+                return value;
+            }
+        });
+
+        /**
+         * union 合并<相同类型>的流，但可以是多条
+         */
+        DataStream<Double> unionStream = isDouble.union(notDouble);
+
+
+        stream3.print().setParallelism(1);
         env.execute("test");
     }
 
